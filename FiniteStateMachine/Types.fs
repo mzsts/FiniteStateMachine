2ff383ce82fc1a8c96f51476ef6bed0a27e66a4d
Maxim <mzsts@inbox.ru>
2023-07-25 22:19:11 +0700

    added State and Event types

---
+++ FiniteStateMachine/Types.fs
@@ -0,0 +1,25 @@
+﻿namespace FiniteStateMachine
+
+type FsmEvent =
+    | APP_PASSIVE_OPEN
+    | APP_ACTIVE_OPEN
+    | APP_SEND
+    | APP_CLOSE
+    | APP_TIMEOUT
+    | ARCV_SYN
+    | RCV_ACK
+    | RCV_SYN_ACK
+    | RCV_FIN
+    | RCV_FIN_ACK
+
+type FsmState =
+    | CLOSED
+    | SYN_SENT
+    | SYN_RCVD
+    | ESTABLISHED
+    | CLOSE_WAIT
+    | LAST_ACK
+    | FIN_WAIT_1
+    | FIN_WAIT_2
+    | CLOSING
+    | TIME_WAIT
