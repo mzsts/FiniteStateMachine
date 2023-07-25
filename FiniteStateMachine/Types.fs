1d43a62a780cd8cf006996d57ba7c8bb5121a873
Maxim <mzsts@inbox.ru>
2023-07-25 22:19:43 +0700

    added method to convert string to Event type

--- FiniteStateMachine/Types.fs
+++ FiniteStateMachine/Types.fs
@@ -11,6 +11,20 @@ type FsmEvent =
     | RCV_SYN_ACK
     | RCV_FIN
     | RCV_FIN_ACK
+        
+    static member fromString str =
+        match str with
+        | "APP_PASSIVE_OPEN" -> Ok APP_PASSIVE_OPEN
+        | "APP_ACTIVE_OPEN" -> Ok APP_ACTIVE_OPEN
+        | "APP_SEND" -> Ok APP_SEND
+        | "APP_CLOSE" -> Ok APP_CLOSE
+        | "APP_TIMEOUT" -> Ok APP_TIMEOUT
+        | "ARCV_SYN" -> Ok ARCV_SYN
+        | "RCV_ACK" -> Ok RCV_ACK
+        | "RCV_SYN_ACK" -> Ok RCV_SYN_ACK
+        | "RCV_FIN" -> Ok RCV_FIN
+        | "RCV_FIN_ACK" -> Ok RCV_FIN_ACK
+        | _ -> Error @$"Invalid input. Can not match event '{str}'"
 
 type FsmState =
     | CLOSED
