a269b6d9a97d3f6f8eacd6daabc249f61c60ce93
Maxim <mzsts@inbox.ru>
2023-07-26 22:36:07 +0700

    added mapping from input str to events

---
+++ FiniteStateMachine/Mapper.fs
@@ -0,0 +1,23 @@
+﻿namespace FiniteStateMachine
+
+module Mapper =
+
+    let private mapToEvents listStr =
+
+        let rec mapToEvents' listStr acc =
+            match listStr with
+            | x::xs ->
+                match Event.FromString x with
+                | Ok event -> mapToEvents' xs (acc @ [event])
+                | Error errorValue -> Error errorValue
+            | [] -> Ok acc
+
+        mapToEvents' listStr []
+
+    let GetEventsFromString (str: string) : Result<Event list, string> = 
+        str.Split(",")
+        |> Array.map(fun x -> x.Trim())
+        |> Array.where(fun x -> x.Length <> 0)
+        |> Array.toList
+        |> mapToEvents
+
